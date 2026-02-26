bd59d37b51d335ec121455fc4ced89a0f4f6918c
micsamuel <mykelsown@gmail.com>
2026-02-26 16:57:59 +0000

    Was able to create the code for converting the string to lower case

--- 5kyu/FirstNonRepeatingCharacter/FNRC.go
+++ 5kyu/FirstNonRepeatingCharacter/FNRC.go
@@ -1,6 +1,18 @@
 package fiveKyu
 
+import "strings"
+
 func Fnrc(str string) string{
-	
+
+	// Conversion of the string to lowercase
+    result := []rune(str)
+
+    for i, r := range result {
+        if r >= 'A' && r <= 'Z' {
+            result[i] = r + ('a' - 'A')
+        }
+    }
+
+    convertedStr := string(result)
 
 }
